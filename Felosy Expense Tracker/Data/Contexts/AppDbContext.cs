a56608ebe10bc94122bad3ee8348eccd3af6131a
Hasan Ali <hasan.aly.dev@gmail.com>
2025-05-19 12:58:03 +0300

    setting up the DBContext
    DBContext and first db migration

---
+++ Felosy Expense Tracker/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Felosy_Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Felosy_Expense_Tracker.Data.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
