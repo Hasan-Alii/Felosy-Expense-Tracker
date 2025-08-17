835fcb3be0c7a93695e306ca09161f05228c5459
Hasan Ali <hasan.aly.dev@gmail.com>
2025-08-17 16:16:36 +0300

    Add Experements controller for test and update config

---
+++ Felosy Expense Tracker/Controllers/Experements.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Felosy_Expense_Tracker.Controllers
+{
+    public class Experements : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
